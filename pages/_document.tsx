dd2b3be30cb267f80e49de5ff5b58fd19c0a3d62
Arsalan Hussain <arsalanmhussain32@gmail.com>
2023-01-23 13:09:44 +0500

    fix favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/darsi-logo.png" />
+
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
