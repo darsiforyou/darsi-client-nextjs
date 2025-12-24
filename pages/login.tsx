04969d98b491b4e4f93a7c27fec0b42c0fa4d4c2
darsiforyou <126070632+darsiforyou@users.noreply.github.com>
2025-12-24 20:51:23 +0500

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -63,7 +63,7 @@ export default function Login() {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`;
+    window.location.href = `https://darsi-backend.vercel.app/api/auth/google/callback`;
   };
 
   return (
