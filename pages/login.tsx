36c378607d335e0489a45f3fc8c584428e7635d0
darsiforyou <126070632+darsiforyou@users.noreply.github.com>
2025-12-24 18:48:46 +0500

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -85,7 +85,7 @@ export default function Login() {
 
   // Uses last signed-in Google account
   const googleLoginDefault = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}api/auth/google`;
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`;
   };
 
   // Forces Google to ask for email & password
